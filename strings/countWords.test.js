b65749ff71a13302d3461275f39d159af5d36268
Reinoptland <reinoptland@gmail.com>
2023-02-03 07:56:33 +0100

    countWords

---
+++ strings/countWords.test.js
@@ -0,0 +1,34 @@
+function countWords(word) {
+  // Implement me
+  return word.replace(/\s+/g, " ").trim().split(" ").length;
+}
+
+describe("countWords", () => {
+  test("should count the words in a piece of text", () => {
+    const input = "The quick brown fox jumps over the lazy dog";
+    const wordCount = countWords(input);
+
+    expect(wordCount).toBe(9);
+  });
+
+  test("BONUS: should work with double spaces in a sentence", () => {
+    const input = "The quick brown  fox jumps over  the lazy dog";
+    const wordCount = countWords(input);
+
+    expect(wordCount).toBe(9);
+  });
+
+  test("BONUS: should work with spaces at the end of a sentence", () => {
+    const input = "The quick brown fox jumps over the lazy dog  ";
+    const wordCount = countWords(input);
+
+    expect(wordCount).toBe(9);
+  });
+
+  test("BONUS: should work with spaces at the start of a sentence", () => {
+    const input = "  The quick brown fox jumps over the lazy dog";
+    const wordCount = countWords(input);
+
+    expect(wordCount).toBe(9);
+  });
+});
