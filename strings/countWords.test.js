6b00aebaa56ddcc0f499477da990365228233a4c
Reinoptland <reinoptland@gmail.com>
2023-02-03 07:57:52 +0100

    countWords basic

--- strings/countWords.test.js
+++ strings/countWords.test.js
@@ -1,6 +1,6 @@
 function countWords(word) {
   // Implement me
-  return word.replace(/\s+/g, " ").split(" ").length;
+  return word.split(" ").length;
 }
 
 describe("countWords", () => {
@@ -11,7 +11,7 @@ describe("countWords", () => {
     expect(wordCount).toBe(9);
   });
 
-  test("BONUS: should work with double spaces in a sentence", () => {
+  test.skip("BONUS: should work with double spaces in a sentence", () => {
     const input = "The quick brown  fox jumps over  the lazy dog";
     const wordCount = countWords(input);
 
