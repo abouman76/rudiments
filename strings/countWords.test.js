51001de559474966ed568e0dbc986e8a01d8d3fe
Reinoptland <reinoptland@gmail.com>
2023-02-03 07:57:23 +0100

    countWords with double spaces

--- strings/countWords.test.js
+++ strings/countWords.test.js
@@ -1,6 +1,6 @@
 function countWords(word) {
   // Implement me
-  return word.replace(/\s+/g, " ").trim().split(" ").length;
+  return word.replace(/\s+/g, " ").split(" ").length;
 }
 
 describe("countWords", () => {
@@ -18,14 +18,14 @@ describe("countWords", () => {
     expect(wordCount).toBe(9);
   });
 
-  test("BONUS: should work with spaces at the end of a sentence", () => {
+  test.skip("BONUS: should work with spaces at the end of a sentence", () => {
     const input = "The quick brown fox jumps over the lazy dog  ";
     const wordCount = countWords(input);
 
     expect(wordCount).toBe(9);
   });
 
-  test("BONUS: should work with spaces at the start of a sentence", () => {
+  test.skip("BONUS: should work with spaces at the start of a sentence", () => {
     const input = "  The quick brown fox jumps over the lazy dog";
     const wordCount = countWords(input);
 
