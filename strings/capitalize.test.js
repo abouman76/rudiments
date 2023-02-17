07d2e3151eb7ebd0c8c72179f842dc71e68de993
abouman76 <abouman76@gmail.com>
2023-02-17 15:34:33 +0100

    both exercises are done

--- strings/capitalize.test.js
+++ strings/capitalize.test.js
@@ -1,5 +1,28 @@
 function capitalize(word) {
   // implement me
+  // return word.charAt(0).toUpperCase() + word.slice(1);
+
+  // What is the input "the quick brown"
+  // cut the string into individual words and put them into an array
+  // iterate over each word['the', 'quick', 'brown']
+  // when iterating a word, capitalize the first letter['The', 'Quick', 'Brown']
+  // concat array to a string with spaces in between `The Quick Brown`
+  // return the string as output
+
+//   const words = word.split(' ');
+//   //console.log("what is words?", words);
+//
+//   for (let i = 0; i < words.length; i++) {
+//     words[i] = words[i][0].toUpperCase() + words[i].substring(1)
+//   }
+//   return words.join(' ');
+// }
+
+ return word.split(' ').map((word) => {
+    return word[0].toUpperCase() + word.substring(1)
+  })
+     .join(' ')
+
 }
 
 describe.skip("capitalize", () => {
@@ -10,7 +33,7 @@ describe.skip("capitalize", () => {
     expect(output).toBe("Hello");
   });
 
-  test.skip("BONUS: should capitalize all words in a sentence", () => {
+  test("BONUS: should capitalize all words in a sentence", () => {
     const input = "the quick brown fox jumps over the lazy dog";
     const output = capitalize(input);
 
--- strings/countWords.test.js
+++ strings/countWords.test.js
@@ -1,8 +1,20 @@
-function countWords() {
+function countWords(words) {
   // Implement me
+  // input: "  The quick brown fox jumps over the lazy dog"
+  // take of white space at the start of the string "The quick brown fox jumps over the lazy dog"
+  // create an array ['The', ]
+  // count the words: 9
+  // output = 9 words
+
+  //.replace(/\s+/g, ' ')
+
+  const word = words.trim().split(/\s+/g);
+  // console.log('Spatie verwijdert?', word.length)
+    return word.length
+
 }
 
-describe.skip("countWords", () => {
+describe("countWords", () => {
   test("should count the words in a piece of text", () => {
     const input = "The quick brown fox jumps over the lazy dog";
     const wordCount = countWords(input);
@@ -10,21 +22,21 @@ describe.skip("countWords", () => {
     expect(wordCount).toBe(9);
   });
 
-  test.skip("BONUS: should work with double spaces in a sentence", () => {
+  test("BONUS: should work with double spaces in a sentence", () => {
     const input = "The quick brown  fox jumps over  the lazy dog";
     const wordCount = countWords(input);
 
     expect(wordCount).toBe(9);
   });
 
-  test.skip("BONUS: should work with spaces at the end of a sentence", () => {
+  test("BONUS: should work with spaces at the end of a sentence", () => {
     const input = "The quick brown fox jumps over the lazy dog  ";
     const wordCount = countWords(input);
 
     expect(wordCount).toBe(9);
   });
 
-  test.skip("BONUS: should work with spaces at the start of a sentence", () => {
+  test("BONUS: should work with spaces at the start of a sentence", () => {
     const input = "  The quick brown fox jumps over the lazy dog";
     const wordCount = countWords(input);
 
