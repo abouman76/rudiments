3779df89cb837f9346cc15d8952c8b97db0bd50d
Reinoptland <reinoptland@gmail.com>
2023-03-01 08:56:54 +0100

    Ignore unsupported options in paymentOptions

---
+++ objects/paymentOptions.test.js
@@ -0,0 +1,43 @@
+function paymentOptions(paymentProviders) {
+  // start with an empty array
+  const arrayOfOptions = [];
+
+  // iterate over al keys of the object of providers
+  for (let paymentProvider in paymentProviders) {
+    // for each provider, check if they are supported
+    if (paymentProviders[paymentProvider] === true) {
+      // if they are supported add them to the array of options
+      arrayOfOptions.push(paymentProvider);
+    }
+  }
+
+  // get al payment providers except the last one and seperate them with a ", "
+  // for example ideal, mastercard
+  const optionsSeperatedByComma = arrayOfOptions
+    .slice(0, arrayOfOptions.length - 1)
+    .join(", ");
+  // get the last option for example paypal
+  const lastOption = arrayOfOptions[arrayOfOptions.length - 1];
+  return `You can pay with ${optionsSeperatedByComma} and ${lastOption}`;
+}
+
+describe("paymentOptions", () => {
+  test("should return a string with payments options", () => {
+    const input = { ideal: true, mastercard: true, paypal: true };
+    const output = paymentOptions(input);
+    expect(output).toBe("You can pay with ideal, mastercard and paypal");
+  });
+
+  test("BONUS: should ignore payment options which are not supported", () => {
+    const input = {
+      ideal: true,
+      klarna: true,
+      mastercard: false,
+      paypal: true,
+      afterpay: false,
+    };
+
+    const output = paymentOptions(input);
+    expect(output).toBe("You can pay with ideal, klarna and paypal");
+  });
+});
