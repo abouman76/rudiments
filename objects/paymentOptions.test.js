35ceb5f31e9dddc303417e361be36b0300afdf68
Reinoptland <reinoptland@gmail.com>
2023-03-01 08:58:51 +0100

    paymentOptions exercise

--- objects/paymentOptions.test.js
+++ objects/paymentOptions.test.js
@@ -1,24 +1,8 @@
 function paymentOptions(paymentProviders) {
-  // start with an empty array
-  const arrayOfOptions = [];
-
-  // iterate over al keys of the object of providers
-  for (let paymentProvider in paymentProviders) {
-    // add them to the array of options
-    arrayOfOptions.push(paymentProvider);
-  }
-
-  // get al payment providers except the last one and seperate them with a ", "
-  // for example ideal, mastercard
-  const optionsSeperatedByComma = arrayOfOptions
-    .slice(0, arrayOfOptions.length - 1)
-    .join(", ");
-  // get the last option for example paypal
-  const lastOption = arrayOfOptions[arrayOfOptions.length - 1];
-  return `You can pay with ${optionsSeperatedByComma} and ${lastOption}`;
+  // implement me
 }
 
-describe("paymentOptions", () => {
+describe.skip("paymentOptions", () => {
   test("should return a string with payments options", () => {
     const input = { ideal: true, mastercard: true, paypal: true };
     const output = paymentOptions(input);
