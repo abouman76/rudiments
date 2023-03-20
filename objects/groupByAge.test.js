01f832dc8cd45baa16d1379c558341a54c12cd1a
abouman76 <abouman76@gmail.com>
2023-03-20 09:25:28 +0100

    first excersise done

--- objects/groupByAge.test.js
+++ objects/groupByAge.test.js
@@ -1,8 +1,38 @@
+/*
+ * - create a variabe for the array method function
+ * - loop through the array with a array function method. map() or Reduce()?
+ * - filter-if statement on each livecycle/age: kids, teens, adults, retirees
+ * - push each statement into the param of the reduce by "adding" the group and pushing it into this new group within the object.
+ * - return the accumulator
+ * - return the variable of the reduce()
+ * - output should be: output = {kids: [1], teens: [2], adults: [5], retirees: [2]}
+ *
+ */
+
+
+
 function groupByAge(people) {
-  // Implement me!
+  const groups = people.reduce((acc, groupedByAge) => {
+    console.log('reduce', groupedByAge.age)
+    if(groupedByAge.age <= 9) {
+      acc.kids.push(groupedByAge);
+    } else if(groupedByAge.age >= 10 && groupedByAge.age <= 19) {
+      acc.teens.push(groupedByAge);
+    } else if(groupedByAge.age > 16 &&  groupedByAge.age <= 65) {
+      acc.adults.push(groupedByAge);
+    } else {
+      acc.retirees.push(groupedByAge);
+    }
+
+    return acc;
+
+  }, { kids: [], teens: [], adults: [], retirees: [] })
+    console.log('grouped', groups);
+
+  return groups;
 }
 
-describe.skip("groupByAge", () => {
+describe("groupByAge", () => {
   test("should return an object that groups people by their age as kids, teens, adults or retirees", () => {
     const input = [
       { name: "John Smith", age: 25 },
@@ -45,7 +75,7 @@ describe.skip("groupByAge", () => {
 
     const output = groupByAge(input);
     // in this data set there are no teens and no retirees so the
-    // output should be an object with arrays for kids, adultslike this:
+    // output should be an object with arrays for kids, adults like this:
     /*
       {
         kids: [],
